e6bff913f63f7c86c92d170f3daa455b35e7412f
jjAugust <junjie.sop@gmail.com>
2017-09-27 12:31:25 +0800

    xiong

---
+++ xiong/Insert35.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Insert35 {
+
+
+
+public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+	return nums[0]*nums[1]*nums[nums.length-1]>nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]?nums[0]*nums[1]*nums[nums.length-1]:nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3];
+    }
+	 }
